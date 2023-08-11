d6a6b00c3c4c6b484bb4d9228b31082d12696af3
Maulana Adam Sahid <masadamsahid@gmail.com>
2023-08-11 19:34:15 +0700

    fix home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,12 @@
+import { redirect } from "next/navigation";
 
 export default function Home() {
+  
+  redirect('/products');
+  
   return (
     <main className="">
-      Test
+      You're being redirected. Please wait!
     </main>
   )
 }
